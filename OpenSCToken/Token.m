d508acf2924c0e216b7c9ffeb40a0667494c14e6
Frank Morgner <frankmorgner@gmail.com>
2018-03-18 00:28:59 +0100

    fixed licensing text

--- OpenSCToken/Token.m
+++ OpenSCToken/Token.m
@@ -8,13 +8,13 @@
  * Software Foundation, either version 3 of the License, or (at your option)
  * any later version.
  *
- * pcsc-relay is distributed in the hope that it will be useful, but WITHOUT
+ * OpenSCToken is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * pcsc-relay.  If not, see <http://www.gnu.org/licenses/>.
+ * OpenSCToken.  If not, see <http://www.gnu.org/licenses/>.
  */
 @import os.log;
 @import Foundation;
